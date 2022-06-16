53ff81c21f7afad0592f96a17f45efced105e593
Vaibhav Girish Ginnalwar <97450767+Vaibhav0702@users.noreply.github.com>
2022-06-16 19:27:54 +0530

    footer done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,9 @@
 
-import { Heading } from '@chakra-ui/react'
+
 import React from 'react'
 import { BsFacebook, BsInstagram, BsTwitter, BsYoutube } from 'react-icons/bs'
+
+import { FaGooglePay } from "react-icons/fa"
 
 import "./Footer.css"
 
@@ -117,16 +119,16 @@ const Footer = () => {
 
                             <p className='fheading'>FOLLOW US</p>
 
-                            <div style={{display:"flex" , gap:"20px" ,marginTop:"20px"  }} >
+                            <div style={{ display: "flex", gap: "20px", marginTop: "20px" }} >
 
-                               <a href="https://www.instagram.com/pharmeasyapp/"  target="blank" > <BsInstagram  size="40px"  color='#10847e'   /></a>
+                                <a href="https://www.instagram.com/pharmeasyapp/" target="blank" > <BsInstagram size="40px" color='#10847e' /></a>
 
-                               <a href=""> <BsFacebook  size="40px" color='#10847e'   /></a>
-                               
-                                  <a href=""> <BsYoutube size="40px"  color='#10847e'  /></a>
-                                 
-<a href=""><BsTwitter  size="40px" color='#10847e'    /></a>
-                                  
+                                <a href="https://www.facebook.com/pharmeasy/" target="blank"     > <BsFacebook size="40px" color='#10847e' /></a>
+
+                                <a href="https://www.youtube.com/channel/UCDats_DLX-bGZH3-KGu8JhA" target="blank"      > <BsYoutube size="40px" color='#10847e' /></a>
+
+                                <a href="https://www.twitter.com/pharmeasyapp/" target="blank"      ><BsTwitter size="40px" color='#10847e' /></a>
+
 
 
                             </div>
@@ -139,6 +141,44 @@ const Footer = () => {
 
                     </div>
 
+
+                    <div>
+
+                        <p className='fheading'>Our Payment Partners</p>
+
+
+                        <div className="fclower"   >
+
+                            <div className="fclleft" >
+
+                                <img width="80px" src="https://www.logo.wine/a/logo/Google_Pay/Google_Pay-Logo.wine.svg" alt="" />
+
+                                <img width="80px" src="https://www.logo.wine/a/logo/PhonePe/PhonePe-Logo.wine.svg" alt="" />
+
+                                <img width="80px" src="https://www.logo.wine/a/logo/Paytm/Paytm-Logo.wine.svg" alt="" />
+
+                                <img width="80px" src="https://www.logo.wine/a/logo/Amazon_Pay/Amazon_Pay-Logo.wine.svg" alt="" />
+
+                                <img width="80px" src="https://www.logo.wine/a/logo/MobiKwik/MobiKwik-Logo.wine.svg" alt="" />
+
+                                <img width="80px" src="https://www.logo.wine/a/logo/Airtel_India/Airtel_India-Logo.wine.svg" alt="" />
+
+                                <img width="80px" src="https://www.logo.wine/a/logo/Mastercard/Mastercard-Logo.wine.svg" alt="" />
+
+                                <img  width="80px" src="https://www.logo.wine/a/logo/Visa_Inc./Visa_Inc.-Logo.wine.svg" alt="" />
+                               
+
+
+                            </div>
+
+                            <div style={{color:"gray"}}>
+                                <p>© 2022 PharmEasy. All Rights Reserved</p>
+                            </div>
+
+
+                        </div>
+
+                    </div>
 
 
 
