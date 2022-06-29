fbe43625c8685b5742895eea2a2f99c12684068c
Vaibhav Girish Ginnalwar <97450767+Vaibhav0702@users.noreply.github.com>
2022-06-29 13:17:59 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -11,7 +11,9 @@ import { store } from "./Redux/store";
 
 
 axios.defaults.baseURL = "https://dataserverbank.herokuapp.com";
+
 // axios.defaults.baseURL = "http://localhost:8080";
+
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -28,4 +30,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
